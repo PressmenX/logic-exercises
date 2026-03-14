dc48ce36317f4ec5d519042de1dfc52d00ccdb0f
Daffa <affah2212@gmail.com>
2026-03-14 14:24:34 +0700

    feat: implement palindrome checker functionality

---
+++ 01-string-manipulation/palindrome-checker.js
@@ -0,0 +1,16 @@
+/**
+ * Tantangan: Palindrome Checker
+ * Deskripsi: Mengecek apakah sebuah kata tetap sama jika dibalik.
+ * Topik: String
+ * Level: Easy
+ */
+const isPalindrome = str => {
+    const text1 = str.toLowerCase().replace(/\s+/g, '')
+    const textToArray = text1.split("").reverse()
+    const text2 = textToArray.join("")
+    return text1 === text2
+}
+
+console.log(isPalindrome("katak") );
+console.log(isPalindrome("hello"));
+console.log(isPalindrome("A man a plan a canal Panama"));
